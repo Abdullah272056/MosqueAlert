10191cee15f0f458e68962921fdd0ce9a727d2ab
Abdullah272056 <abdullah272056@gmail.com>
2021-04-23 03:14:34 +0600

    update function

--- app/src/main/java/com/example/mosquealert/CustomAdapter.java
+++ app/src/main/java/com/example/mosquealert/CustomAdapter.java
@@ -1,33 +1,19 @@
 package com.example.mosquealert;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-
-import static android.content.Context.ALARM_SERVICE;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder>{
 EditText waktoNameEditText,minuteEditText;
@@ -76,89 +62,10 @@ Button cancelButton ,saveButton;
         }else {
 
         }
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
 
 
-//        holder.switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//                    currentMilliSecond=System.currentTimeMillis();
-//
-//                    Toast.makeText(context, "check", Toast.LENGTH_SHORT).show();
-//
-//                    long status = databaseHelper.updateData(new Notes(allNotes.get(position).getId(),
-//                            allNotes.get(position).getStartTime(),allNotes.get(position).getEntTime(),
-//                            allNotes.get(position).getStartTimeMilli(),
-//                            allNotes.get(position).getEndTimeMilli(),
-//                            allNotes.get(position).getAudioMode(),
-//                            1));
-//
-//                    if (status == 1){
-//                        allNotes.clear();
-//                        allNotes.addAll(databaseHelper.getAllNotes());
-//                        //notifyDataSetChanged();
-//
-//                        if (currentMilliSecond<=Long.parseLong(allNotes.get(position).getStartTimeMilli())){
-//                            Intent intent=new Intent(context, NotificationReceiver.class);
-//                            intent.putExtra("notificationRequestCode",allNotes.get(position).getId());
-//                            intent.putExtra("TargetTimeMilliSecond",allNotes.get(position).getStartTimeMilli());
-//                            alarmIntent= PendingIntent.getBroadcast(context,
-//                                    allNotes.get(position).getId(),intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                            //get ALARM_SERVICE from SystemService
-//                            alarm= (AlarmManager) context.getSystemService(ALARM_SERVICE);
-//                            //alarm set
-//                            alarm.set(AlarmManager.RTC_WAKEUP,Long.parseLong(allNotes.get(position).getStartTimeMilli()),alarmIntent);
-//
-//
-//                            alarmIntent= PendingIntent.getBroadcast(context,
-//                                    allNotes.get(position).getId()+100,intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                            //get ALARM_SERVICE from SystemService
-//                            alarm= (AlarmManager) context.getSystemService(ALARM_SERVICE);
-//                            int minuteInt =Integer.parseInt(allNotes.get(position).getEntTime());
-//                            //alarm set
-//                            alarm.set(AlarmManager.RTC_WAKEUP,(Long.parseLong(allNotes.get(position).getStartTimeMilli())+(minuteInt*60000)),alarmIntent);
-//                        }else {
-//                            Toast.makeText(context, "Current Time is bigger than target time", Toast.LENGTH_SHORT).show();
-//                        }
-//                        Toast.makeText(context, String.valueOf(allNotes.get(position).getId()), Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//                else{
-//                    long status = databaseHelper.updateData(new Notes(allNotes.get(position).getId(),
-//                            allNotes.get(position).getStartTime(),allNotes.get(position).getEntTime(),
-//                            allNotes.get(position).getStartTimeMilli(),
-//                            allNotes.get(position).getEndTimeMilli(),
-//                            allNotes.get(position).getAudioMode(),
-//                            0));
-//                    if (status == 1){
-//                        allNotes.clear();
-//                        allNotes.addAll(databaseHelper.getAllNotes());
-//                        //notifyDataSetChanged();
-//
-//                      Intent intent=new Intent(context, NotificationReceiver.class);
-//                        alarmIntent= PendingIntent.getBroadcast(context,
-//                                allNotes.get(position).getId(),intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                        alarm= (AlarmManager) context.getSystemService(ALARM_SERVICE);
-//                     alarm.cancel(alarmIntent);
-//
-//                        alarmIntent= PendingIntent.getBroadcast(context,
-//                                allNotes.get(position).getId()+100,intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                        //get ALARM_SERVICE from SystemService
-//                        alarm= (AlarmManager) context.getSystemService(ALARM_SERVICE);
-//                        alarm.cancel(alarmIntent);
-//
-//                        //  Toast.makeText(context, "Successfully Updated", Toast.LENGTH_SHORT).show();
-//                    }
-//                    Toast.makeText(context, String.valueOf(allNotes.get(position).getId()), Toast.LENGTH_SHORT).show();
-//
-//                }
-//            }
-//        });
+
+
 
     }
 
@@ -179,108 +86,23 @@ Button cancelButton ,saveButton;
             endTextView=itemView.findViewById(R.id.endTimeTextViewId);
             this.onContactClickListener=onContactClickListener;
             itemView.setOnClickListener(this);
-            switchButton.setOnClickListener(this);
+           //switchButton.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            onContactClickListener.onContactClick(getAdapterPosition());
+            onContactClickListener.onContactClick(getAdapterPosition(),switchButton);
         }
     }
 
 
 
     public  interface  OnContactClickListener{
-        void onContactClick(int position);
+        void onContactClick(int position, Switch switchButton);
+
+        ;
     }
 
 
-//    private void CustomDialog(){
-//
-//        final BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(context);
-//        bottomSheetDialog.setContentView(R.layout.input_layout);
-//        bottomSheetDialog.setCanceledOnTouchOutside(false);
-//
-//        saveButton=bottomSheetDialog.findViewById(R.id.saveButtonId);
-//        cancelButton=bottomSheetDialog.findViewById(R.id.cancelButtonId);
-//        minuteEditText=bottomSheetDialog.findViewById(R.id.minuteEditTextId);
-//        waktoNameEditText=bottomSheetDialog.findViewById(R.id.waktoNameEditTextId);
-//        final TimePicker startTimePicker =bottomSheetDialog.findViewById(R.id.timePickerId);
-//
-//        saveButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                String waktoName=waktoNameEditText.getText().toString();
-//                String endMinute=minuteEditText.getText().toString();
-//                if (TextUtils.isEmpty(waktoName)){
-//                    waktoNameEditText.setError("Enter wakto name");
-//                    waktoNameEditText.requestFocus();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(endMinute)){
-//                    minuteEditText.setError("Enter minute name");
-//                    minuteEditText.requestFocus();
-//                    return;
-//                }
-//
-//
-//                DateFormat dateFormat = new SimpleDateFormat("hh:mm aa");
-//                int hour=startTimePicker.getCurrentHour();
-//                int minute=startTimePicker.getCurrentMinute();
-//                Calendar startTimeCalender=Calendar.getInstance();
-//                startTimeCalender.set(Calendar.HOUR_OF_DAY,hour);
-//                startTimeCalender.set(Calendar.MINUTE,minute);
-//                startTimeCalender.set(Calendar.SECOND,0);
-//                String startTimeString = dateFormat.format(startTimeCalender.getTime()).toString();
-//
-//                startTimeMilliSecond=startTimeCalender.getTimeInMillis();
-//
-//                int addMinute=Integer.parseInt(endMinute);
-//                endTimeMilliSecond=startTimeMilliSecond+(addMinute*60000);
-//
-//                int id=dataBaseHelper.insertData(new Notes(startTimeString,endTime,String.valueOf(startTimeMilliSecond),
-//                        String.valueOf(endTimeMilliSecond),"www",1));
-//                if (id!=-1){
-//
-//                    Intent intent=new Intent(MainActivity.this, NotificationReceiver.class);
-//                    intent.putExtra("notificationRequestCode",id);
-//                    intent.putExtra("TargetTimeMilliSecond",startTimeMilliSecond);
-//                    alarmIntent= PendingIntent.getBroadcast(MainActivity.this,
-//                            (int) id,intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                    //get ALARM_SERVICE from SystemService
-//                    alarm= (AlarmManager) getSystemService(ALARM_SERVICE);
-//                    //alarm set
-//                    alarm.set(AlarmManager.RTC_WAKEUP,startTimeMilliSecond,alarmIntent);
-//
-//                    alarmIntent= PendingIntent.getBroadcast(MainActivity.this,
-//                            (int) id+100,intent,PendingIntent.FLAG_CANCEL_CURRENT);
-//                    //get ALARM_SERVICE from SystemService
-//                    alarm= (AlarmManager) getSystemService(ALARM_SERVICE);
-//                    int minuteInt =Integer.parseInt(minuteEditText.getText().toString());
-//                    //alarm set
-//                    alarm.set(AlarmManager.RTC_WAKEUP,(startTimeMilliSecond+(minuteInt*60000)),alarmIntent);
-//
-//
-//
-//                    Toast.makeText(MainActivity.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
-//                    loadData();
-//                    bottomSheetDialog.dismiss();
-//                }else {
-//                    Toast.makeText(MainActivity.this, "insert fail", Toast.LENGTH_SHORT).show();
-//                    bottomSheetDialog.dismiss();
-//                }
-//
-//
-//
-//            }
-//        });
-//
-//        cancelButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                bottomSheetDialog.dismiss();
-//            }
-//        });
-//        bottomSheetDialog.show();
-//    }
+
 }
--- app/src/main/java/com/example/mosquealert/MainActivity.java
+++ app/src/main/java/com/example/mosquealert/MainActivity.java
@@ -8,7 +8,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -27,6 +31,13 @@ RecyclerView recyclerView;
     CustomAdapter customAdapter;
     private List<Notes> dataList;
 
+    // CustomDialog
+    EditText waktoNameEditText,minuteEditText;
+    Button cancelButton ,saveButton;
+    AlarmManager alarm;
+    PendingIntent alarmIntent;
+    String endTime;
+
     CustomAdapter.OnContactClickListener onContactClickListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +47,9 @@ RecyclerView recyclerView;
         recyclerView=findViewById(R.id.recyclerViewId);
         dataBaseHelper=new DataBaseHelper(MainActivity.this);
         dataBaseHelper.getWritableDatabase();
-        loadData(onContactClickListener);
+        loadData();
     }
-    private void loadData(CustomAdapter.OnContactClickListener onContactClickListener){
+    private void loadData(){
         dataList  = new ArrayList<>();
         dataList = dataBaseHelper.getAllNotes();
 
@@ -67,7 +78,115 @@ RecyclerView recyclerView;
     }
 
     @Override
-    public void onContactClick(int position) {
+    public void onContactClick(int position, Switch switchButton) {
         Toast.makeText(this, "position", Toast.LENGTH_SHORT).show();
+        switchButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "swwww", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CustomDialog(position);
+    }
+
+        private void CustomDialog(final int position){
+
+
+        final BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(MainActivity.this);
+        bottomSheetDialog.setContentView(R.layout.input_layout);
+        bottomSheetDialog.setCanceledOnTouchOutside(false);
+
+        saveButton=bottomSheetDialog.findViewById(R.id.saveButtonId);
+        cancelButton=bottomSheetDialog.findViewById(R.id.cancelButtonId);
+        minuteEditText=bottomSheetDialog.findViewById(R.id.minuteEditTextId);
+        waktoNameEditText=bottomSheetDialog.findViewById(R.id.waktoNameEditTextId);
+        final TimePicker startTimePicker =bottomSheetDialog.findViewById(R.id.timePickerId);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String waktoName=waktoNameEditText.getText().toString();
+                String endMinute=minuteEditText.getText().toString();
+                Long startTimeMilliSecond,endTimeMilliSecond;
+                if (TextUtils.isEmpty(waktoName)){
+                    waktoNameEditText.setError("Enter wakto name");
+                    waktoNameEditText.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(endMinute)){
+                    minuteEditText.setError("Enter minute name");
+                    minuteEditText.requestFocus();
+                    return;
+                }
+
+
+                DateFormat dateFormat = new SimpleDateFormat("hh:mm aa");
+                int hour=startTimePicker.getCurrentHour();
+                int minute=startTimePicker.getCurrentMinute();
+                Calendar startTimeCalender=Calendar.getInstance();
+                startTimeCalender.set(Calendar.HOUR_OF_DAY,hour);
+                startTimeCalender.set(Calendar.MINUTE,minute);
+                startTimeCalender.set(Calendar.SECOND,0);
+                String startTimeString = dateFormat.format(startTimeCalender.getTime()).toString();
+
+                startTimeMilliSecond=startTimeCalender.getTimeInMillis();
+
+                int addMinute=Integer.parseInt(endMinute);
+                endTimeMilliSecond=startTimeMilliSecond+(addMinute*60000);
+
+
+
+                long id=dataBaseHelper.updateData(new Notes(dataList.get(position).getId(),waktoName,startTimeString,endMinute,String.valueOf(startTimeMilliSecond),
+                        String.valueOf(endTimeMilliSecond),"www",1));
+                if (id==1){
+                    bottomSheetDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "update success", Toast.LENGTH_SHORT).show();
+                    loadData();
+                }
+
+//                if (id==1){
+//
+//                    Intent intent=new Intent(MainActivity.this, NotificationReceiver.class);
+//                    intent.putExtra("notificationRequestCode",id);
+//                    intent.putExtra("TargetTimeMilliSecond",startTimeMilliSecond);
+//                    alarmIntent= PendingIntent.getBroadcast(MainActivity.this,
+//                            (int) id,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+//                    //get ALARM_SERVICE from SystemService
+//                    alarm= (AlarmManager) getSystemService(ALARM_SERVICE);
+//                    //alarm set
+//                    alarm.set(AlarmManager.RTC_WAKEUP,startTimeMilliSecond,alarmIntent);
+//
+//                    alarmIntent= PendingIntent.getBroadcast(MainActivity.this,
+//                            (int) id+100,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+//                    //get ALARM_SERVICE from SystemService
+//                    alarm= (AlarmManager) getSystemService(ALARM_SERVICE);
+//                    int minuteInt =Integer.parseInt(minuteEditText.getText().toString());
+//                    //alarm set
+//                    alarm.set(AlarmManager.RTC_WAKEUP,(startTimeMilliSecond+(minuteInt*60000)),alarmIntent);
+//
+//
+//
+//                    Toast.makeText(MainActivity.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
+//                    loadData();
+//                    bottomSheetDialog.dismiss();
+//                }else {
+//                    Toast.makeText(MainActivity.this, "insert fail", Toast.LENGTH_SHORT).show();
+//                    bottomSheetDialog.dismiss();
+//                }
+
+
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+            }
+        });
+        bottomSheetDialog.show();
     }
 }
